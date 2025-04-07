cc7eab5554490e7520069ad66a66c28a2dc8ac3a
mypiett <147983106+mypiett@users.noreply.github.com>
2025-04-07 23:08:38 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const userController = require('../controller/user.controller');
+const { UserController } = require('../controller/user.controller');
+
 const router = express.Router();
+const userController = new UserController();
 
-router.use('/users', userController);
+router.get('/users', userController.getAllUsers.bind(userController));
+router.get('/users/:id', userController.getUserById.bind(userController));
+router.post('/users', userController.createUser.bind(userController));
+router.put('/users/:id', userController.updateUser.bind(userController));
+router.delete('/users/:id', userController.deleteUser.bind(userController));
 
-module.exports = router; 
+module.exports = router;
